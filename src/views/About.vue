564c45c00df0f7abb6cecdfba83a8ca6219a0696
Khairul Abdi Dongoran <kabdi384@gmail.com>
2020-12-14 00:58:13 +0700

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, voluptatum nesciunt impedit debitis sunt atque consequatur quia minus asperiores! Veritatis eligendi consectetur dolorum pariatur eum reiciendis amet distinctio non culpa!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, voluptatum nesciunt impedit debitis sunt atque consequatur quia minus asperiores! Veritatis eligendi consectetur dolorum pariatur eum reiciendis amet distinctio non culpa!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, voluptatum nesciunt impedit debitis sunt atque consequatur quia minus asperiores! Veritatis eligendi consectetur dolorum pariatur eum reiciendis amet distinctio non culpa!</p>
   </div>
 </template>
